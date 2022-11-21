1a809bf2b3fd93a8ed3b11b10bb118aa4e50a5dd
icyclestick <icebebe111@gmail.com>
2022-11-21 14:04:36 +0000

    11/21/22 progress even just a lil bit

--- client/src/components/create-chicken.component.js
+++ client/src/components/create-chicken.component.js
@@ -1,67 +1,133 @@
-import { useState } from "react"
+import { Component } from "react";
 import axios from 'axios';
-export default function CreateUser(props){
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+export default class CreateChicken extends Component{
+  constructor(props){
+    super(props);
 
-    const [formData, setFormData] = useState({
-        tag: "",
-        type: "",
-        weight: "",
-        targetWeight: ""
-      })
-    
-      function handleChange(event){
-        console.log(event)
-        // const {name, value} = event.target
-        console.log(event.value)
-        setFormData(prevData => {
-          return {
-            ...prevData,
-            [event.target.name]: event.target.value
-          }
-        })
-      }
+    this.onChangeTag = this.onChangeTag.bind(this)
+    this.onChangeType = this.onChangeType.bind(this)
+    this.onChangeWeight = this.onChangeWeight.bind(this)
+    this.onChangeTargetWeight = this.onChangeTargetWeight.bind(this)
+    this.onChangeUsername = this.onChangeUsername.bind(this)
+    this.onChangeDate = this.onChangeDate.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
 
-      function handleSubmit(event){
-        event.preventDefault()
-        const userData = {
-          tag: formData.tag,
-          type: formData.type,
-          weight: formData.weight,
-          targetWeight: formData.targetWeight
-      }
-        axios.post(`http://localhost:4000/api/create`, userData)
-        .then(res => console.log(res.data))
-        setFormData({
-          tag: "",
-          type: "",
-          weight: "",
-          targetWeight: ""
-        })
-      }
+    this.state = {
+      username: '',
+      tag: '',
+      type: '',
+      weight: '',
+      targetWeight: '',
+      date: new Date(),
+      users: []
+    }
+  }
 
+  componentDidMount(){
+    this.setState({
+      users: ['test user'],
+      username: 'test user'
+    })
+  }
+
+  onChangeTag(e){
+    this.setState({
+      tag: e.target.value
+    });
+  }
+  onChangeType(e){
+    this.setState({
+      type: e.target.value
+    });
+  }
+  onChangeWeight(e){
+    this.setState({
+      weight: e.target.value
+    });
+  }
+  onChangeTargetWeight(e){
+    this.setState({
+      targetWeight: e.target.value
+    });  
+  }
+  onChangeDate(date){
+    this.setState({
+      date: date
+    });  
+  }
+  onChangeUsername(e){
+    this.setState({
+      username: e.target.value
+    });  
+  }
+  
+  
+  handleSubmit(event){
+    event.preventDefault()
+    const userData = {
+      tag: this.state.tag,
+      type: this.state.type,
+      weight: this.state.weight,
+      targetWeight: this.state.targetWeight,
+      username: this.state.username,
+      date: this.state.date
+    }
+    axios.post(`http://localhost:4000/api/create`, userData)
+    .then(res => console.log(res.data))
+    this.setState({
+      tag: "",
+      type: "",
+      weight: "",
+      targetWeight: ""
+
+    })
+    console.log(userData)
+    window.location = '/'
+  }
+
+  render() {
     return (
-      
-<form onSubmit={handleSubmit}>
-    <div class="grid gap-6 mb-6 md:grid-cols-2">
-        <div>
-            <label htmlFor="tag" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Tag</label>
-            <input name="tag" onChange={handleChange} value={formData.tag}type="text" id="tag" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="0000" required=""/>
-        </div>
-        <div>
-            <label htmlFor="type" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Type</label>
-            <input name="type" onChange={handleChange} value={formData.type}type="text" id="type" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Darag Chicken" required=""/>
-        </div>
-        <div>
-            <label htmlFor="weight" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Weight</label>
-            <input name="weight" onChange={handleChange} value={formData.weight}type="text" id="weight" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="15kg" required=""/>
-        </div>  
-        <div>
-            <label htmlFor="target_weight" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Target Weight</label>
-            <input name="targetWeight" onChange={handleChange} value={formData.targetWeight}type="text" id="target_weight" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="20kg" required=""/>
-        </div>
-      <button type="submit" onChange={handleSubmit} class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
-    </div>
-</form>
-
-    )
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="user" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Username: </label>
+        <select ref="userInput"className="form-control" name="user" required value={this.state.username} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500">
+          {
+            this.state.users.map(function(user){
+              return <option 
+              key={user}
+              value={user}
+              >{user}
+              </option>
+            })
+          }
+        </select>
+          <div class="grid gap-6 mb-6 md:grid-cols-2">
+              <div>
+                  <label htmlFor="tag" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Tag</label>
+                  <input name="tag" onChange={this.onChangeTag} value={this.state.tag}type="text" id="tag" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="0000" required=""/>
+              </div>
+              <div>
+                  <label htmlFor="type" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Type</label>
+                  <input name="type" onChange={this.onChangeType} value={this.state.type}type="text" id="type" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Darag Chicken" required=""/>
+              </div>
+              <div>
+                  <label htmlFor="weight" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Weight</label>
+                  <input name="weight" onChange={this.onChangeWeight} value={this.state.weight}type="text" id="weight" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="15kg" required=""/>
+              </div>  
+              <div>
+                  <label htmlFor="target_weight" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Target Weight</label>
+                  <input name="targetWeight" onChange={this.onChangeTargetWeight} value={this.state.targetWeight}type="text" id="target_weight" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="20kg" required=""/>
+              </div>
+              <div>
+                <DatePicker 
+                  selected={this.state.date}
+                  onChange={this.onChangeDate}
+                />
+              </div>
+            <button type="submit" onChange={this.handleSubmit} class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
+          </div>
+      </form>
+    );
+  }
 }
