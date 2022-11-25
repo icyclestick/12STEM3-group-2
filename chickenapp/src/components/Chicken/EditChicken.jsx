f5fad70d8823ae20a8a70a6ad433af9b1dd51f2a
icyclestick <icebebe111@gmail.com>
2022-11-25 14:48:36 +0000

    11/25/22 fix fix

--- chickenapp/src/components/Chicken/EditChicken.jsx
+++ chickenapp/src/components/Chicken/EditChicken.jsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router"
+import { getChickenById } from "../../api"
 export const EditChicken = () => {
+    const [editChicken, setEditChicken] = useState({
+        username: '',
+        tag: '',
+        type: '',
+        weight: '',
+        targetWeight: '',
+        date: new Date(),
+        users: []
+    })
     const route = useParams();
     // ## TODO Call to web api and put supply the input elements
-    return <>{route.id}</>
+   useEffect(() => {
+        getChickenById().then(response => {
+            setEditChicken({
+                username: response,
+                tag: response,
+                type: response,
+                weight: response,
+                targetWeight: response,
+                date: new Date(response),
+                users: [response]
+            })
+        }).catch(error => {
+            console.log(error)
+        })
+    }, [])
+    return <>{route.params.id}</>
 }
