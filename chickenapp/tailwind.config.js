db2b8ccb304931c7f9764a17d087ad5841c75c1a
icyclestick <icebebe111@gmail.com>
2022-12-05 14:39:46 +0000

    12/5/22 help colloquium tom

--- chickenapp/tailwind.config.js
+++ chickenapp/tailwind.config.js
@@ -1,12 +1,11 @@
 module.exports = {
+  darkMode: "media",
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-]
-}
+  plugins: [require("flowbite/plugin")],
+};
