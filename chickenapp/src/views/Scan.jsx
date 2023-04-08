eb8b3fe0ca6c556896b7156bc50f891a356fc267
icyclestick <icebebe111@gmail.com>
2023-04-09 00:06:25 +0800

    09/04/23 deploying frontend

--- chickenapp/src/views/Scan.jsx
+++ chickenapp/src/views/Scan.jsx
@@ -35,15 +35,15 @@ export const Scan = () => {
 
     return <>
         <div className="scan-wrapper">
-            <h3>Scanned By WebCam Code: {scanResultWebCam}</h3>
-            <h3>Qr Code Scan by Web Cam</h3>
+            <h3 class="text-4xl font-extrabold dark:text-white">Scanned By WebCam Code: {scanResultWebCam}</h3>
+            <h3 class="text-4xl font-extrabold dark:text-white">Qr Code Scan by Web Cam</h3>
             <button onClick={filterChickenByQr} class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">CONFIRM</button>
             <QrReader
                 className='qr-image-wrapper'
                 scanDelay={300}
                 videoStyle={{
-                    width: '25%',
-                    height: '25%',
+                    width: '100%',
+                    height: '100%',
                     border: "5px red solid"
                 }}
                 videoContainerStyle={{ border: "5px red solid" }}
--- client/src/components/params.js
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom";
-
-export default function Params() {
-  let params = useParams();
-  return (
-    {number: params.id}
-  )
-}
