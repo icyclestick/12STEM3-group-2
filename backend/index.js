48f4bbfaf508349fd9271f8e1cac5085f5e5a743
icyclestick <icebebe111@gmail.com>
2023-04-22 23:09:21 +0800

    22/04/23 push

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const serverless = require("serverless-http");
+
+require("dotenv").config();
+
+module.exports.handler = serverless(app);
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established succesfully");
+});
+
+const chickenRouter = require("./routes/chicken");
+const userRouter = require("./routes/users");
+
+app.use("/chicken", chickenRouter);
+app.use("/users", userRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
