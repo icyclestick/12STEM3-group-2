5d040b461e34797ba1b1f132b7b8e922b9b1b9bf
icyclestick <icebebe111@gmail.com>
2023-04-23 19:44:22 +0800

    23/04/23 parseiso bs >;( pt2

--- chickenapp/src/index.js
+++ chickenapp/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
 import { createUserApi } from "./api";
